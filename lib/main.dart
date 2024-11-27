e41326afc113b2b9179c77958d3bd6d1a85e0729
Davide La Manna <Davide.La-Manna@Vodafone.com>
2024-11-28 00:58:52 +0100

    core: minor clean up code and new icon

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Lo Zac App',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -81,13 +81,13 @@ class _MyHomePageState extends State<MyHomePage> {
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.onPrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.onPrimary,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
               icon: Icon(Icons.home_outlined), label: "Home"),
@@ -96,54 +96,66 @@ class _MyHomePageState extends State<MyHomePage> {
               icon: Icon(Icons.people_outline), label: "Chi Siamo"),
         ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              'Zac Menu',
-              style: Theme.of(context).textTheme.headlineLarge,
-            ),
-            // Text(
-            //   '$_counter',
-            //   style: Theme
-            //       .of(context)
-            //       .textTheme
-            //       .headlineMedium,
-            // ),
-            SizedBox(
-                width: double.infinity,
-                height: 600,
-                child: ListView.builder(
-                  itemCount: 4,
-                  itemBuilder: (BuildContext context, int index) {
-                    return card("blalalalal", "Pasta",
-                        "This is bla bla bal aalalll ala ", "10 euro", context);
-                  },
-                ))
-          ],
-        ),
+      body: Container(
+        color: const Color(0xffF4F8FC),
+        child: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            //
+            // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+            // action in the IDE, or press "p" in the console), to see the
+            // wireframe for each widget.
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 16.0),
+                child: Text(
+                  'Zac Menu',
+                  style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                      fontFamily: 'CenturyGothic', fontWeight: FontWeight.bold),
+                ),
+              ),
+              // Text(
+              //   '$_counter',
+              //   style: Theme
+              //       .of(context)
+              //       .textTheme
+              //       .headlineMedium,
+              // ),
+              SizedBox(
+                  width: double.infinity,
+                  height: 500,
+                  child: ListView.builder(
+                    itemCount: 4,
+                    itemBuilder: (BuildContext context, int index) {
+                      return card(
+                          "blalalalal",
+                          "Pasta",
+                          "This is bla bla bal aalalll ala ",
+                          "10 euro",
+                          context);
+                    },
+                  ))
+            ],
+          ),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
       ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.deepOrangeAccent,
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
@@ -167,13 +179,24 @@ Widget card(String image, String title, String description, String price,
           child: Column(
             children: [
               Text(title,
-                  style: Theme.of(context)
-                      .textTheme
-                      .headlineLarge
-                      ?.copyWith(color: Colors.deepOrangeAccent),
+                  style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                      color: Colors.deepOrangeAccent,
+                      fontFamily: 'CenturyGothic'),
                   textAlign: TextAlign.start),
-              Text(description),
-              Text(price),
+              Text(
+                description,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyMedium
+                    ?.copyWith(fontFamily: 'CenturyGothic'),
+              ),
+              Text(
+                price,
+                style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                    color: Colors.deepOrangeAccent,
+                    fontFamily: 'CenturyGothic',
+                    fontWeight: FontWeight.bold),
+              ),
             ],
           )));
 }
